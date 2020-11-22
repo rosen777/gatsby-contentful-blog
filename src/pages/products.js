a6f0c7b3d181a8bbd8c8906a0249924e8c786030
Rosen Toshev <rosen.toshev@expend.io>
2020-11-22 17:36:43 +0200

    Added 404, blog, index, and proucts pages. Developed a Layout, Navbar and Footer components to wrap these pages.

---
+++ src/pages/products.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Layout from '../components/layout'
+
+
+export default class products extends Component {
+    render() {
+        return (
+            <Layout>
+                <h1>This is our product page</h1>
+            </Layout>
+        )
+    }
+}
+
