bbcda893cc0d5e42593ad0a0bc33c1f2e72a7e64
Rosen Toshev <rosen.toshev@expend.io>
2021-01-02 21:19:07 +0200

    Added fluid and fixed gatsby images as well as images with maxWidth that sets the various sizes in which the images are offered.

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,10 +5,36 @@
  */
 
 module.exports = {
-  plugins: [
-    {
-      resolve: `gatsby-plugin-styled-components`,
-      /* Your config settings here */
+  siteMetadata:  {
+    title: "Gatsby TUtorial",
+    description: "some random description",
+    author: "@rosentoshev",
+    data: ["item 1", "item 2"],
+    person: {
+      name: "peter",
+      age: 32
     }
+  }
+  ,
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    `gatsby-transformer-sharp`, 
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`, 
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/src/posts/`,
+      },
+    },
   ],
 }
---
+++ src/pages/images.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../components/layout'
+import Images from '../examples/Images'
+
+function images() {
+    return (
+        <Layout>
+            <Images />
+        </Layout>
+    )
+}
+
+export default images
